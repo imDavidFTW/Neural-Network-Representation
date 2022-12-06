650ccbf833e882f70269e8c833c32e37e03ab16c
imDavidFTW <102111499+imDavidFTW@users.noreply.github.com>
2022-12-06 06:36:03 -0600

    Update Filters.java
    added lowerbound and upperbound variables to filter

--- Filters.java
+++ Filters.java
@@ -6,13 +6,13 @@ public class Filters{
     protected int depth;
     protected int m;
     protected ArrayList<double[][]> appliedFilters;
-    public Filters(int m, int depth){
+    public Filters(int m, int depth, double lowerBound, double upperBound){
         appliedFilters = new ArrayList<>();
         this.m = m;
         this.depth = depth;
         for(int d = 0; d < depth; d++){
             double[][] filter = new double[m][m];
-            double[] randoms = ThreadLocalRandom.current().doubles(m*m, -1.0, 5).toArray();
+            double[] randoms = ThreadLocalRandom.current().doubles(m*m, lowerBound, upperBound).toArray();
             for(int i = 0; i < m; i++){
                 for(int j = i * m; j < i * m + m; j++){
                     filter[i][j%m] = Math.round(randoms[j]);
