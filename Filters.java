b55552e721d3177871e71d22dc7a2d6b044c80a8
David McKinney <dmckinn7@cbu.edu>
2022-12-07 18:02:08 -0600

    Added a toString function to the filters class, so the filters and their values can be seen

--- Filters.java
+++ Filters.java
@@ -21,5 +21,44 @@ public class Filters{
             appliedFilters.add(filter);
         }
     }
+
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        for(int i = 0; i < this.depth; i++)
+        {
+            s.append("[");
+            for(int j = 0; j < this.m; j++)
+            {
+                s.append("[");
+                for(int k = 0; k < this.m; k++){
+                    if(k == this.m - 1){
+                        s.append(this.appliedFilters.get(i)[j][k]);   
+                    }
+                    else
+                        s.append(this.appliedFilters.get(i)[j][k] + ", ");
+                }
+                if(j == this.m - 1)
+                    s.append("]");
+                else
+                    s.append("], ");
+                if (this.m - 1 != j){
+                    s.append(System.getProperty("line.separator"));
+                }
+            }
+            if(i == this.depth - 1)
+                s.append("]");
+            else
+                s.append("], ");
+            if(this.depth - 1 != i)
+            {
+                s.append(System.getProperty("line.separator"));
+                s.append(System.getProperty("line.separator"));
+            }
+        }
+        s.append("]");
+        return s.toString();
+    }
 }
 
--- Test.java
+++ Test.java
@@ -2,22 +2,10 @@
 public class Test{
     public static void main(String[] args){
         long startTime = System.nanoTime();
-        Filters filters = new Filters(11, 96, -1.0, 3.0);
-        for(int i = 0; i < filters.depth; i++)
-        {
-            for(int j = 0; j < filters.m; j++)
-            {
-                for(int k = 0; k < filters.m; k++)
-                    System.out.print(filters.appliedFilters.get(i)[j][k] + "  ");
-            }
-            System.out.println("  ");
-            System.out.println("  ");
-            System.out.println(i);
-            System.out.println("  ");
-            System.out.println("  ");   
-        }
+        Filters filters = new Filters(3, 5, -1.0, 3.0);
+        System.out.println(filters.toString());
         long endTime = System.nanoTime();
-        long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+        double duration = (double)((double)(endTime - startTime)/1000000.0)/1000.0;  //divide by 1000000 to get milliseconds.
         System.out.println(duration);
     } 
 }
