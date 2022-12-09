686524ad9303d3c18126786bc0f9fce71d76446e
Abdala-Jabase <abdala.jabase@gmail.com>
2022-12-09 11:48:34 -0600

    Added Constructor

--- Matrix.java
+++ Matrix.java
@@ -2,5 +2,9 @@
 //will also perform the convolutions on the image and the max/avg pooling 
 //to extract important features from the image
 public class Matrix {
-    
+    int[][] matrix;
+
+    public Matrix(int m, int n) {
+        matrix = new int[m][n];
+    }
 }
