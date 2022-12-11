543f1b11ae16ceb3c738217c9d87ad1f1b340b5b
Abdala-Jabase <abdala.jabase@gmail.com>
2022-12-11 02:55:50 -0600

    ImageToMatrix method, helper methods added

--- Matrix.java
+++ Matrix.java
@@ -1,10 +1,40 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 //class will be used to convert an input image to a matrix
 //will also perform the convolutions on the image and the max/avg pooling 
 //to extract important features from the image
-public class Matrix {
-    int[][] matrix;
+public class Matrix<T> {
+    T[][] matrix;
 
     public Matrix(int m, int n) {
-        matrix = new int[m][n];
+        matrix = (T[][]) new Object[m][n];
+    }
+
+    public T get(int i, int j) {
+        return matrix[i][j];
+    }
+
+    public void set(int i, int j, T val) {
+        this.matrix[i][j] = val;
+    }
+
+    public static Matrix<Boolean> ImageToBooleanMatrix(String pathname) {
+        try {
+            File pathToFile = new File(pathname);
+            BufferedImage image = ImageIO.read(pathToFile);
+            Matrix<Boolean> currMatrix = new Matrix<>(image.getHeight(), image.getWidth());
+            for (int i = 0; i < image.getHeight(); i++) {
+                for (int j = 0; j < image.getWidth(); j++) {
+                    currMatrix.set(i, j, image.getRGB(j, i) < -9000000 ? false : true);
+                }
+            }
+            return currMatrix;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return new Matrix<>(0, 0);
     }
 }
--- Test.java
+++ Test.java
@@ -1,11 +1,8 @@
+import java.util.Arrays;
+
 //Class used for testing
-public class Test{
-    public static void main(String[] args){
-        long startTime = System.nanoTime();
-        Filters filters = new Filters(3, 5, -1.0, 1.0);
-        System.out.println(filters.toString());
-        long endTime = System.nanoTime();
-        double duration = (double)((double)(endTime - startTime)/1000000.0)/1000.0;  //divide by 1000000 to get milliseconds.
-        System.out.println(duration);
-    } 
+public class Test {
+    public static void main(String[] args) {
+
+    }
 }
