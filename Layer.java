3f724d95f77da1818be53dd92690206a80abafc2
David McKinney <dmckinn7@cbu.edu>
2022-12-16 22:12:06 -0600

    added in the Layer class

---
+++ Layer.java
@@ -0,0 +1,38 @@
+public class Layer {
+    public int numOfNeurons;
+    public int prevLayerSize;
+    public Neuron[] layer;
+    public double bias;
+    public Layer(int numOfNeurons, int prevLayerSize){
+        this.numOfNeurons = numOfNeurons;
+        this.prevLayerSize = prevLayerSize;
+        this.layer = new Neuron[numOfNeurons];
+        for(int i = 0; i < numOfNeurons; i++){
+            this.layer[i] = new Neuron(prevLayerSize);
+        }
+        this.bias = Math.random();
+    }
+
+    public static Layer[] hiddenLayers(int numOfLayers, int[] neuronsPerLayer, Matrix<Boolean> m){
+        Layer[] layers = new Layer[numOfLayers];
+        layers[0] = new Layer(m);
+        for(int i = 1; i < numOfLayers; i++){
+            layers[i] = new Layer(neuronsPerLayer[i], neuronsPerLayer[i - 1]);
+        }
+        return layers;
+    }
+
+    public Layer(Matrix<Boolean> m){
+        this.layer = new Neuron[m.getRows()*m.getColumns()];
+        this.numOfNeurons = m.getRows()*m.getColumns();
+        this.prevLayerSize = 0;
+        this.bias = 0;
+        int counter = 0;
+        for(int i = 0; i < m.getRows(); i++){
+            for(int j = 0; j < m.getColumns(); j++){
+                this.layer[counter++] = new Neuron(0, m.get(i, j) ? 1 : 0);
+            }
+        }
+    }
+}
+
--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -5,38 +5,5 @@
 //The result of the weights at the end of training will 
 //be the neural network used for testing 
 public class NeuralNetwork {
-    
-    public Neuron[] Layer(int numOfNeurons, int prevLayerSize){
-        Neuron[] layer = new Neuron[numOfNeurons + 1];
-        for(int i = 0; i < numOfNeurons; i++){
-            layer[i] = new Neuron(prevLayerSize + 1);
-        }
-        layer[numOfNeurons] = new Neuron(numOfNeurons);
-        double bias = layer[numOfNeurons].weights[0];
-        for(int i = 1; i < layer[numOfNeurons].weights.length; i++){
-            layer[numOfNeurons].weights[i] = bias;
-        }
-        layer[numOfNeurons].output = 1;
-        return layer;
-    }
-    public Neuron[][] hiddenLayer(int numOfLayers, int[] neuronsPerLayer){
-        Neuron[][] layers = new Neuron[numOfLayers][neuronsPerLayer.length];
-        layers[0] = Layer(neuronsPerLayer[0], 0);
-        for(int i = 1; i < numOfLayers; i++){
-            layers[i] = Layer(neuronsPerLayer[i], neuronsPerLayer[i - 1]);
-        }
-        return layers;
-    }
-    
-    public static Neuron[] booleanMatrixToInputLayer(Matrix<Boolean> m){
-        Neuron[] inputLayer = new Neuron[m.getRows()*m.getColumns()];
-        int counter = 0;
-        for(int i = 0; i < m.getRows(); i++){
-            for(int j = 0; j < m.getColumns(); j++){
-                inputLayer[counter++] = new Neuron(0, m.get(i, j) ? 1 : 0);
-            }
-        }
-        return inputLayer;
-    }
-
+        
 }
