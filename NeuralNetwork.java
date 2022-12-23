a5d81c500a3ea369101fa02ebf10c50f02230b0b
imDavidFTW <davidmck123987@cbu.edu>
2022-12-23 05:59:46 -0600

    wrote plans for forward propegation

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -6,18 +6,33 @@
 //be the neural network used for testing 
 public class NeuralNetwork {
     public Layer[] layers;
-    public double error;
-    private Layer outputLayer;
-    private double[] expectedResultsForInput = new double[] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    private double[] softmaxLayer = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0};//Use this to store the values obtained from the softmax function
+    //public double error;
+    //private double[] expectedResultsForInput = new double[] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
     public NeuralNetwork(Layer[] layers){
 
     }
 
+    //For more clarification go to power point 7 I sent you
     private void ForwardPropegation(){
-
+        //For the input of each neuron sum the weights times the outputs of the previous neurons plus the bias
+        //For the output of each neuron use the sigmoid activation function 1/(1+e^(-x)) with x being the input value calculated
+        //Then calculate your values for the double[] softmaxLayer using the softmax function and the output values from last layer of the NN
+        //Softmax makes a vector of probabilites
     }
 
-    private void BackWardPropegation(){
+
+
+
+
+
+
+
+
+
+
+
+    /*private void BackWardPropegation(){
         
     }
 
@@ -30,5 +45,5 @@ public class NeuralNetwork {
         for(int i = 0; i < length; i++){
             error += 0.5*Math.pow((outputLayer.layer[i].output - this.expectedResultsForInput[i]), 2);
         }
-    }
+    }*/
 }
