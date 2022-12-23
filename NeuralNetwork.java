03690b108af6322eb51b6e432ad66837ae979655
imDavidFTW <davidmck123987@cbu.edu>
2022-12-23 01:09:42 -0600

    started setting up the NN class

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -5,5 +5,30 @@
 //The result of the weights at the end of training will 
 //be the neural network used for testing 
 public class NeuralNetwork {
+    public Layer[] layers;
+    public double error;
+    private Layer outputLayer;
+    private double[] expectedResultsForInput = new double[] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    public NeuralNetwork(Layer[] layers){
+
+    }
+
+    private void ForwardPropegation(){
+
+    }
+
+    private void BackWardPropegation(){
         
+    }
+
+    private void inputNum(int n){
+        this.expectedResultsForInput[n - 1] = 0.91;
+    }
+
+    public void err(Layer outputLayer){
+        int length = outputLayer.layer.length;
+        for(int i = 0; i < length; i++){
+            error += 0.5*Math.pow((outputLayer.layer[i].output - this.expectedResultsForInput[i]), 2);
+        }
+    }
 }
